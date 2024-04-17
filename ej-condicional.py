2a5dc6d057324f5466c30e3c0966cbacd625d05f
Cristian Vargas (Malvadapapa) <cristian.v62@gmail.com>
2024-04-17 00:02:00 -0300

    first commit

---
+++ ej-condicional.py
@@ -0,0 +1,8 @@
+primera_letra = input('ingrese una letra: ')
+segunda_letra = input('ingrese otra letra: ')
+
+if primera_letra == segunda_letra:
+    print('Las letras son iguales')
+else:
+    print('Las letras no son iguales')
+
